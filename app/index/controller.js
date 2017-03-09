68dbde3829b9a2ca9645ac990c7b60afd7af146b
russvetsper <russvetsper@gmail.com>
2017-03-09 10:36:09 -0800

    populate controller js for example of computed propersties and observers

--- app/index/controller.js
+++ app/index/controller.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true
+  isDisabled: true,
+
+  emailAddress: '',
+
+  actualEmailAddress: Ember.computed('emailAddress', function() {
+    console.log('acctualEmailAddress function called: ', this.get('emailAddress'));
+  }),
+
+  emailAddressChanged: Ember.observer('emailAddress', function() {
+    console.log('observer is called', this.get('emailAddress')); 
+  })
 });
